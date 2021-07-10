d72684bca2b2a561c68e8e08c835d4dd4c9118e2
Oisín Mac Fhearaí <denpashogai@gmail.com>
2021-07-10 02:46:59 +0100

    Ch9.6... four from the top and two small please Carol.

--- app/Ch9.hs
+++ app/Ch9.hs
@@ -1,6 +1,6 @@
 module Ch9 where
 
--- 9.3
+-- 9.3: Countdown!
 data Op = Add | Sub | Mul | Div
 
 instance Show Op where
@@ -36,3 +36,45 @@ values (App _ l r) = values l ++ values r
 eval :: Expr -> [Int]
 eval (Val x) = [x | x > 0]
 eval (App op l r) = [apply op x y | x <- eval l, y <- eval r, valid op x y]
+
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x:xs) = yss ++ map (x:) yss
+  where yss = subs xs
+
+interleave :: a -> [a] -> [[a]]
+interleave x [] = [[x]]
+interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms = foldr (concatMap . interleave) [[]]
+
+choices :: [a] -> [[a]]
+choices = concatMap perms . subs
+
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = elem (values e) (choices ns) && eval e == [n]
+
+split :: [a] -> [([a],[a])]
+split [] = []
+split [_] = []
+split (x:xs) = ([x],xs) : [(x:ls,rs) | (ls,rs) <- split xs]
+
+exprs :: [Int] -> [Expr]
+exprs [] = []
+exprs [n] = [Val n]
+exprs ns = [e | (ls,rs) <- split ns,
+                l <- exprs ls,
+                r <- exprs rs,
+                e <- combine l r]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+ops :: [Op]
+ops = [Add, Sub, Mul, Div]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns,
+                      e <- exprs ns',
+                      eval e == [n]]
--- test/Ch9Spec.hs
+++ test/Ch9Spec.hs
@@ -15,3 +15,4 @@ spec =
     it "extracts Vals" $ values (App Div (Val 6) (App Add (Val 1) (Val 2))) `shouldBe` [6,1,2]
     it "evals" $ eval (App Div (Val 6) (App Add (Val 1) (Val 2))) `shouldBe` [2]
     it "doesn't eval invalid" $ eval (App Div (Val 6) (App Add (Val 1) (Val 4))) `shouldBe` []
+    it "solves a Countdown problem" $ eval (head (solutions [10,2,8,50,100] 155)) `shouldBe` [155]
